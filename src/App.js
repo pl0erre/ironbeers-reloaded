fac079e44a825ecfd1da0bedeea7224f960f7a8d
pl0erre <luca.angona@web.de>
2019-08-13 16:48:09 +0200

    rewriting nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // Requirements
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {Route, Link} from 'react-router-dom';
+
+// Font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBeer } from '@fortawesome/free-solid-svg-icons';
 
 // Pages
 import Home from './Pages/Home';
@@ -17,46 +21,58 @@ import SignUp from './Components/SignUp';
 import ProtectedRoute from './Components/ProtectedRoute';
 import BeerDetails from './Components/BeerDetails';
 
-// Font awesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBeer } from '@fortawesome/free-solid-svg-icons'
-
+// Utilities
+import Auth from './Utils/Auth';
+const auth = new Auth();
 
 // App
-function App() {
-  return (
-    <div className="App">
-      <nav className="App-nav">
-        <Link to="/"><FontAwesomeIcon icon={faBeer} /></Link>
-        <ul>
-          <li><Link to="/auth/login">Login</Link></li>
-          <li><Link to="/auth/signup"><span  className="nav-join">Join us!</span></Link></li>
-          <li><Link to="/auth/logout">Logout</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-        </ul>
-      </nav>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/all" component={AllBeers} />
-      <Route exact path="/add" component={AddBeer} />
-      <Route exact path="/random" component={RandomBeer} />
-      <Route exact path="/details/:id" component={BeerDetails} />
-      <Route exact path="/auth/login" component={Login} />
-      <Route exact path="/auth/signup" component={SignUp} />
-      <Route exact path="/auth/logout" component={Logout} />
-      <ProtectedRoute 
-        redirectUrl='/auth/login' 
-        path="/profile" 
-        component={Profile}
-      />
-      <footer className="App-footer"></footer>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    user: null
+  }
+  componentDidMount() {
+    this.setState({
+        user: auth.getUser()
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav className="App-nav">
+          <Link to="/"><FontAwesomeIcon icon={faBeer} /></Link>
+          <ul>
+            {this.state.user
+              ? 
+              <>
+                <li><Link to="/auth/logout">Logout</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+                <li><span className="user-welcome">Hello {this.state.user.username}</span></li>
+              </>
+              :
+              <>
+                <li><Link to="/auth/login">Login</Link></li>
+                <li><Link to="/auth/signup"><span  className="nav-join">Join us!</span></Link></li>
+              </>
+            }
+          </ul>
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/all" component={AllBeers} />
+        <Route exact path="/add" component={AddBeer} />
+        <Route exact path="/random" component={RandomBeer} />
+        <Route exact path="/details/:id" component={BeerDetails} />
+        <Route exact path="/auth/login" component={Login} />
+        <Route exact path="/auth/signup" component={SignUp} />
+        <Route exact path="/auth/logout" component={Logout} />
+        <ProtectedRoute 
+          redirectUrl='/auth/login' 
+          path="/profile" 
+          component={Profile}
+        />
+        <footer className="App-footer"></footer>
+      </div>
+    );
+  }
 }
-
-export default App;
-
-
-
-
-
-
